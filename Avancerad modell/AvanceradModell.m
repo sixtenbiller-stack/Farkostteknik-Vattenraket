d58cd740b555c13d71969c8dc88654f49abcb1c5
Sixten <sixtenbiller@gmail.com>
2026-02-10 20:27:08 +0100

    Avancerad modell utkast

---
+++ Avancerad modell/AvanceradModell.m
@@ -0,0 +1,20 @@
+clear all;
+close all;
+
+flaskVolym = 1.5/1000;
+projektionsArea = ((0.0881/2)^2)*pi;
+torrMassa = 0.107;
+munstycksArea = ((0.0205/2)^2)*pi;
+flaskTryck = 7*100000;
+atmTryck = 1 * 100000;
+vattenVolym = 0.5/1000;
+rhoVatten = 1000;
+cdLuft = 0.75;
+cd = 0.98;
+startVinkel = pi/4;
+
+y0 = [0;0;0;0;vattenVolym;flaskTryck];
+
+tspan = [0,5];
+
+[t,resultat] = ode45(@(t,y) Solver(t,y,munstycksArea, cd, rhoVatten, atmTryck, flaskVolym, torrMassa, cdLuft, projektionsArea, startVinkel), tspan, y0);
---
+++ Avancerad modell/Solver.m
@@ -0,0 +1,51 @@
+function dydt = Solver(t,y,munstycksArea,cd,rhoVatten,atmTryck,flaskVolym,torrMassa,cdLuft,projektionsArea,startVinkel)
+    x = y(1); 
+    h = y(2); 
+    vx = y(3); 
+    vy = y(4); 
+    vv = y(5); 
+    p = y(6); 
+    
+    g = 9.81;
+
+    v = sqrt(vx^2 + vy^2);
+    
+    if round(v) < 1
+        ex = cos(startVinkel);
+        ey = sin(startVinkel);
+    else
+        ex = vx / v;
+        ey = vy / v;
+    end
+
+    m = torrMassa + vv*rhoVatten;
+    
+    dragKraft = (1/2)*1.2*(v^2)*cdLuft*projektionsArea;
+
+    if vv > 0
+        dmdt = munstycksArea*cd*sqrt(2*rhoVatten*(p-atmTryck));
+        vattenHastighet = dmdt/(rhoVatten*munstycksArea);
+        motorKraft = dmdt * vattenHastighet;
+        dvvdt = -dmdt / 1000; 
+        luftVolym = flaskVolym - vv;
+        dpdt = (p/luftVolym)*dvvdt; 
+    
+    else %Om vattnet i flaskan är slut
+        motorKraft = 0;
+        dvvdt = 0;
+        dpdt = 0;
+    end
+
+    ftot = motorKraft - dragKraft;
+    fx = ftot * ex;
+    fy = ftot * ey - m*g;
+    ax = fx / m; 
+    ay = fy / m; 
+
+    dydt = [vx;
+            vy;
+            ax;
+            ay;
+            dvvdt;
+            dpdt];
+end
