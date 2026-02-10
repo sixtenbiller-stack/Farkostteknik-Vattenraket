00eef905d2dd668e3d9eacbb0e96bfb263d4728d
Sixten <sixtenbiller@gmail.com>
2026-02-10 20:34:40 +0100

    lade till en enkel plot

--- Avancerad modell/AvanceradModell.m
+++ Avancerad modell/AvanceradModell.m
@@ -18,3 +18,7 @@ y0 = [0;0;0;0;vattenVolym;flaskTryck];
 tspan = [0,5];
 
 [t,resultat] = ode45(@(t,y) Solver(t,y,munstycksArea, cd, rhoVatten, atmTryck, flaskVolym, torrMassa, cdLuft, projektionsArea, startVinkel), tspan, y0);
+plot(resultat(:,1), resultat(:,2));
+
+axis equal;
+grid on;
