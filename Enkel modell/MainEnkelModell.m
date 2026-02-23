21613f9b9102ee5edbaddf290d21941f0422647e
edwinhaoninglo-ops <edwin.haoning.lo@gmail.com>
2026-02-23 15:38:01 +0100

    Update MainEnkelModell.m
    nya värden

--- Enkel modell/MainEnkelModell.m
+++ Enkel modell/MainEnkelModell.m
@@ -1,14 +1,11 @@
-V0 = starthastighet(0.107,0.5,0.89,1000,(0.01^2)*pi,100000,700000,pi/4,0)
+clc; clear all;
+%raketMassa = 0.107 kg
+%vattenMassa = 0.5 kg (exempel för 0.5 liter)
+%cd = 0.89
+%vattenDensitet = 998 kg/m^3
+%A = (0.01^2)*pi (exempel på r = 1 cm)
+%atmTryck = 101300 Pa (14.7 psi motsvarar ca 101,300 Pa)
+%flaskTryck = 700000 Pa (7 bar)
+%startvinkel = pi/4 (45 grader)
 
-%(raketMassa,vattenMassa,cd,vattenDensitet,A,atmTryck, flaskTryck,startvinkel)
-
-% raketMassa = 0.107 kg
-% cd = 0.89
-% vattenDensitet = 0.988...
-% A = pi*r
-% atmTryck = konstant
-
-% Varierande variabler
-% flaskTryck = 7 bar
-% vattenVolym = 
-% vattenMassa =
+[V0, Range] = starthastighet(0.107, 0.5, 0.89, 998, (0.01^2)*pi, 101300, 700000, pi/4)
