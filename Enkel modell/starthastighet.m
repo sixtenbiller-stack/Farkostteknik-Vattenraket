7b2132d27e5b39e4d7a6a761c7e53458020a19be
edwinhaoninglo-ops <edwin.haoning.lo@gmail.com>
2026-02-23 15:38:53 +0100

    Update starthastighet.m
    fixat småfel

--- Enkel modell/starthastighet.m
+++ Enkel modell/starthastighet.m
@@ -1,15 +1,35 @@
-function V0 = starthastighet(raketMassa,vattenMassa,cd,vattenDensitet,A,atmTryck, flaskTryck,startvinkel,fd)
-p = flaskTryck - atmTryck
+function [V0, Range] = starthastighet(raketMassa, vattenMassa, cd, vattenDensitet, A, atmTryck, flaskTryck, startvinkel)
 
-massFlowRate = A*cd*sqrt(2*vattenDensitet*A*p)
-
-vattenVolym = vattenMassa/1000;
-
-flowVelocity = massFlowRate/(vattenDensitet * A)
-
-Range = ((((massFlowRate * vattenMassa)/raketMassa*vattenDensitet*A)^2)*sin(2*startvinkel))/9.82
-
-V0 = ((vattenDensitet * vattenVolym)/massFlowRate)*(((massFlowRate^2)/vattenDensitet*A)-fd-(raketMassa*9.82)/raketMassa)
-
-
+    % Konstant för tyngdacceleration
+    g = 9.8; 
+    
+    % 1. Tryckskillnad (delta P)
+    p = flaskTryck - atmTryck;
+    
+    % 2. Massflöde (massFlowRate) - Extra 'A' under roten är borttaget
+    massFlowRate = A * cd * sqrt(2 * vattenDensitet * p);
+    
+    % 3. Vattnets utgångshastighet (flowVelocity)
+    flowVelocity = massFlowRate / (vattenDensitet * A);
+    
+    % 4. Drivkraft (Thrust)
+    ft = massFlowRate * flowVelocity;
+    
+    % 5. Genomsnittlig massa (m_ave)
+    m_ave = raketMassa + (vattenMassa / 2);
+    
+    % 6. Nettokraft (utan luftmotstånd, fd = 0)
+    f_net = ft - (m_ave * g);
+    
+    % 7. Acceleration (a)
+    a = f_net / m_ave;
+    
+    % 8. Tid för att spruta ut allt vatten (t)
+    t = vattenMassa / massFlowRate;
+    
+    % 9. Starthastighet för kastbanan (V_bottle)
+    V0 = a * t;
+    
+    % 10. Kastlängd (Range)
+    Range = (V0^2 * sin(2 * startvinkel)) / g;
 end
